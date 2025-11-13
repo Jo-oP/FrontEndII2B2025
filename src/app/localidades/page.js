d0a528c076db8d1cb4d28aba646d2b56202c5c20
wagnerferreiraf <professorwagnerferreira@gmail.com>
2025-11-13 14:33:49 -0400

    update

--- src/app/localidades/page.js
+++ src/app/localidades/page.js
@@ -1,24 +1,32 @@
 'use client';
-import { useEffect, useState } from "react"
+import { use, useEffect, useState } from "react"
+import styles from './page.module.css';
 
 export default function Localidades() {
 
     const [paises, setPaises] = useState(null);
     const [status, setStatus] = useState('Carregando lista de países...');
+    const [showTblPaises, setShowTblPaises] = useState(false);
 
     const getPaises = async () => {
         try {
             const response = await fetch('https://servicodados.ibge.gov.br/api/v1/localidades/paises?orderBy=nome');
             if (!response.ok) {
+                setStatus(response.statusText);
                 throw new Error(`Erro ao buscar dados: ${response.statusText}`)
             }
             console.log(response);
             const dados = await response.json();
+            setPaises(dados);
+            setStatus('Lista de países carregada');
             console.log(dados);
-            setStatus(null);
+            // setMsgBtn('Mostrar Países');
+            // setStatus(null);
         } catch (e) {
+            setStatus(`Ocorreu um erro: ${e.message}`);
             console.log(`Ocorreu um erro: ${e.message}`);
         }
+        // exibam id, nome do país e nome da região (subregião/região/nome) em uma tabela.
     }
 
     useEffect(() => {
@@ -29,6 +37,31 @@ export default function Localidades() {
         <div>
             <h1>Localidades</h1>
             {status && <p>{status}</p>}
+            {paises &&
+                <div>
+                    <button type="button" onClick={() => {setShowTblPaises(!showTblPaises)}}>{showTblPaises ? 'Ocultar Paises' : 'Mostrar Países'}</button>
+                    {showTblPaises &&
+                        <table className={styles.tbl}>
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>PAÍS</th>
+                                    <th>REGIÃO</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {paises.map(pais => (
+                                    <tr key={pais.id.M49}>
+                                        <td>{pais.id.M49}</td>
+                                        <td>{pais.nome}</td>
+                                        <td>{pais['sub-regiao'].regiao.nome}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    }
+                </div>
+            }
         </div>
     )
 }
